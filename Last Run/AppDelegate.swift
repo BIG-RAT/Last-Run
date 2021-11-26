ec186bc884577299df01e7e18c6a6b554aa672c7
Leslie Helou <leslie@ccs-indy.com>
2021-11-25 21:43:46 -0500

    Initial Commit

---
+++ Last Run/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Last Run
+//
+//  Created by Leslie Helou on 11/25/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ Last Run/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Last Run
+//
+//  Created by Leslie Helou on 11/25/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
